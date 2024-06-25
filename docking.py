00c3800ca5f8188ae2719784881ddaa9a22bc467
Kauã Ortiz <kauaortizz@gmail.com>
2024-06-25 08:38:20 -0300

    Feat: implemented a flask server to run the functions in mobile app

--- docking.py
+++ docking.py
@@ -7,6 +7,8 @@ from genetic2022 import *
 from bruteforce import *
 import time
 import random
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 def solve_bruteforce(navios, L):
     """
@@ -59,7 +61,10 @@ def solve_ga(N, L, p_count, epochs):
         fitness_history[2].append(best_f[0])
         fitness_history[3].append(best_f[1])
     t1 = time.time()
-    return navios, p, fitness_history, t1-t0
+    best_individual = sorted(p, key=lambda p: p[0])[-1]
+    best_individual_converted = [int(bit) for bit in best_individual]
+
+    return navios, best_individual_converted, fitness_history, t1-t0
 
 def evaluate(result_ga, result_brutefoce = None):
     """
@@ -90,18 +95,47 @@ def evaluate(result_ga, result_brutefoce = None):
     ax.grid(True)
     plt.show()
 
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/solve_ga', methods=['POST'])
+def solve_ga_endpoint():
+    data = request.json
+    N = data['N']
+    L = data['L']
+    p_count = data.get('p_count', 1000)
+    epochs = data.get('epochs', 1000)
+    
+    result_ga = solve_ga(N, L, p_count, epochs)
+    navios, best_individual, fitness_history, t_ga = result_ga
+    
+    response = {
+        "navios": navios,
+        "best_individual": best_individual,
+        "fitness_history": fitness_history,
+        "time_ga": t_ga
+    }
+    
+    return jsonify(response)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
 if __name__ == "__main__":
-    N = int(input("Número de navios: ")) # Número de navios
-    L = int(input("Comprimento do cais: ")) # Comprimento máximo do cais
+    app.run(host='0.0.0.0', port=5000)
+    # N = int(input("Número de navios: ")) # Número de navios
+    # L = int(input("Comprimento do cais: ")) # Comprimento máximo do cais
         
-    p_count = 1000 # Tamanho da população
-    epochs  = 1000 # Número de gerações para testar
+    # p_count = 1000 # Tamanho da população
+    # epochs  = 1000 # Número de gerações para testar
 
-    # Rodar algoritmos
-    result_ga        = solve_ga(N, L, p_count, epochs)
-    result_brutefoce = solve_bruteforce(result_ga[0], L)
+    # # Rodar algoritmos
+    # result_ga        = solve_ga(N, L, p_count, epochs)
+    # result_brutefoce = solve_bruteforce(result_ga[0], L)
 
-    # Plotar resultados
-    print()
-    print("Navios: ", result_ga[0])
-    evaluate(result_ga)#, result_brutefoce)
+    # # Plotar resultados
+    # print()
+    # print("Navios: ", result_ga[0])
+    # evaluate(result_ga)#, result_brutefoce)
